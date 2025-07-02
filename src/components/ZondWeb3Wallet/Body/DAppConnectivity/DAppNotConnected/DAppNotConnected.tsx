54e9b5a3c341c8122c66c8f50231ac3bb0350699
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-07-02 15:02:40 +0400

    feat: adds DAppNotConnected

---
+++ src/components/ZondWeb3Wallet/Body/DAppConnectivity/DAppNotConnected/DAppNotConnected.tsx
@@ -0,0 +1,16 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/UI/Alert";
+import { Unlink } from "lucide-react";
+
+const DAppNotConnected = () => {
+  return (
+    <Alert>
+      <Unlink className="h-4 w-4" />
+      <AlertTitle>Not Connected</AlertTitle>
+      <AlertDescription>
+        Zond Web3 Wallet is not connected to this website.
+      </AlertDescription>
+    </Alert>
+  );
+};
+
+export default DAppNotConnected;
