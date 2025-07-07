be65deb2cf23d995c286909627873e148069f007
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-07-07 14:48:16 +0400

    feat: updates ui

--- src/components/ZondWeb3Wallet/Body/DAppConnectivity/ActiveBrowserTab/ActiveBrowserTab.tsx
+++ src/components/ZondWeb3Wallet/Body/DAppConnectivity/ActiveBrowserTab/ActiveBrowserTab.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/UI/Button";
 import { Card } from "@/components/UI/Card";
 import { Label } from "@/components/UI/Label";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/UI/Tooltip";
+import { ROUTES } from "@/router/router";
 import { useStore } from "@/stores/store";
 import { cva } from "class-variance-authority";
-import { Earth } from "lucide-react";
+import { Earth, Unlink } from "lucide-react";
 import { observer } from "mobx-react-lite";
+import { useNavigate } from "react-router-dom";
 
 const connectivityStatusClasses = cva("h-3 w-3 rounded-full", {
   variants: {
@@ -18,13 +26,20 @@ const connectivityStatusClasses = cva("h-3 w-3 rounded-full", {
 });
 
 const ActiveBrowserTab = observer(() => {
+  const navigate = useNavigate();
   const { dAppRequestStore } = useStore();
-  const { hasDAppConnected, currentTabData } = dAppRequestStore;
+  const { hasDAppConnected, currentTabData, disconnectFromCurrentTab } =
+    dAppRequestStore;
+
+  const disconnect = async () => {
+    await disconnectFromCurrentTab();
+    navigate(ROUTES.HOME);
+  };
 
   return (
     <div className="flex flex-col gap-2">
       <Label className="text-lg">Active browser tab</Label>
-      <Card className="flex flex-col gap-4 p-4">
+      <Card className="flex justify-between gap-4 p-4">
         <div className="flex items-center gap-4">
           <div className="flex items-center gap-2">
             <Card
@@ -47,6 +62,23 @@ const ActiveBrowserTab = observer(() => {
             <span className="text-xm opacity-80">{currentTabData?.title}</span>
           </div>
         </div>
+        {hasDAppConnected && (
+          <Tooltip delayDuration={0}>
+            <TooltipTrigger asChild>
+              <Button
+                className="size-7 hover:bg-accent hover:text-secondary"
+                variant="outline"
+                size="icon"
+                onClick={disconnect}
+              >
+                <Unlink size="16" />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent side="bottom">
+              <Label>Disconnect</Label>
+            </TooltipContent>
+          </Tooltip>
+        )}
       </Card>
     </div>
   );
--- src/components/ZondWeb3Wallet/Body/DAppConnectivity/ConnectivityWithWallet/DAppConnected/DAppConnected.tsx
+++ src/components/ZondWeb3Wallet/Body/DAppConnectivity/ConnectivityWithWallet/DAppConnected/DAppConnected.tsx
@@ -1,53 +1,19 @@
-import { Button } from "@/components/UI/Button";
 import { Card } from "@/components/UI/Card";
-import { ROUTES } from "@/router/router";
 import { useStore } from "@/stores/store";
-import { Unlink } from "lucide-react";
 import { observer } from "mobx-react-lite";
-import { useNavigate } from "react-router-dom";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/UI/Tooltip";
-import { Label } from "@/components/UI/Label";
 import AccountId from "../../../AccountList/AccountId/AccountId";
 
 const DAppConnected = observer(() => {
-  const navigate = useNavigate();
   const { dAppRequestStore, zondStore } = useStore();
   const { zondAccounts } = zondStore;
   const { isLoading } = zondAccounts;
-  const { currentTabData, disconnectFromCurrentTab } = dAppRequestStore;
-
-  const disconnect = async () => {
-    await disconnectFromCurrentTab();
-    navigate(ROUTES.HOME);
-  };
+  const { currentTabData } = dAppRequestStore;
 
   return (
     <Card className="flex flex-col gap-4 p-4">
-      <div className="flex gap-3">
-        <div className="text-sm">
-          The following accounts are connected with this website.
-        </div>
-        <span>
-          <Tooltip delayDuration={0}>
-            <TooltipTrigger asChild>
-              <Button
-                className="size-7 hover:bg-accent hover:text-secondary"
-                variant="outline"
-                size="icon"
-                onClick={disconnect}
-              >
-                <Unlink size="16" />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent side="bottom">
-              <Label>Disconnect</Label>
-            </TooltipContent>
-          </Tooltip>
-        </span>
+      <div className="text-sm">
+        The following accounts are connected, and can interact with this
+        website.
       </div>
       {isLoading ? (
         <div className="flex h-20 w-full animate-pulse items-center justify-between">
