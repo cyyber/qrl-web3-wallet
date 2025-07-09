9144e193f73cb63d07acb9dd2b5364ec8a54a9c1
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-07-09 17:59:28 +0400

    style: updates chainicon

--- src/components/ZondWeb3Wallet/Body/ChainConnectivity/ChainIcon/ChainIcon.tsx
+++ src/components/ZondWeb3Wallet/Body/ChainConnectivity/ChainIcon/ChainIcon.tsx
@@ -10,9 +10,9 @@ const ChainIcon = ({ src, alt }: ChainIconType) => {
   const [hasSrcError, setHasSrcError] = useState(false);
 
   return hasSrcError ? (
-    <Network className="h-6 w-6" />
+    <Network className="h-6 w-6 flex-shrink-0" />
   ) : (
-    <div className="h-6 w-6">
+    <div className="h-6 w-6 flex-shrink-0">
       <img src={src} alt={alt} onError={() => setHasSrcError(true)} />
     </div>
   );
