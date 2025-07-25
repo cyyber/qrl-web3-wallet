3a3ffa860129b6151b07a928826d9bcf485e6c56
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-07-25 11:24:42 +0400

    feat: adds AddZondChainContent

--- src/components/ZondWeb3Wallet/DAppRequest/DAppRequestContent/DAppRequestContentSelection/AddZondChainContent/AddZondChainContent.tsx
+++ src/components/ZondWeb3Wallet/DAppRequest/DAppRequestContent/DAppRequestContentSelection/AddZondChainContent/AddZondChainContent.tsx
@@ -4,11 +4,49 @@ import { useStore } from "@/stores/store";
 import { Check, X } from "lucide-react";
 import { observer } from "mobx-react-lite";
 import AddZondChainInfo from "./AddZondChainInfo/AddZondChainInfo";
+import { BlockchainBaseDataType } from "@/configuration/zondBlockchainConfig";
+import StorageUtil from "@/utilities/storageUtil";
 
 const AddZondChainContent = observer(() => {
-  const { dAppRequestStore } = useStore();
-  const { onPermission, approvalProcessingStatus } = dAppRequestStore;
+  const { dAppRequestStore, zondStore } = useStore();
+  const { addChain } = zondStore;
+  const {
+    dAppRequestData,
+    onPermission,
+    approvalProcessingStatus,
+    setOnPermissionCallBack,
+  } = dAppRequestStore;
   const { isProcessing } = approvalProcessingStatus;
+
+  const addBlockchain = async () => {
+    const onPermissionCallBack = async (hasApproved: boolean) => {
+      if (hasApproved) {
+        const blockchain = dAppRequestData
+          ?.params?.[0] as BlockchainBaseDataType;
+        const { chainFound, updatedChainList } = await addChain(
+          {
+            chainName: blockchain?.chainName,
+            chainId: blockchain?.chainId,
+            nativeCurrency: blockchain?.nativeCurrency,
+            rpcUrls: blockchain?.rpcUrls,
+            blockExplorerUrls: blockchain?.blockExplorerUrls,
+            iconUrls: blockchain?.iconUrls,
+          },
+          {
+            defaultRpcUrl: blockchain?.rpcUrls?.[0] ?? "",
+            defaultBlockExplorerUrl: blockchain?.blockExplorerUrls?.[0] ?? "",
+            defaultIconUrl: blockchain?.iconUrls?.[0] ?? "",
+            isTestnet: false,
+            defaultWsRpcUrl: "http://127.0.0.1:8545",
+            isCustomChain: true,
+          },
+        );
+        if (!chainFound) await StorageUtil.setAllBlockChains(updatedChainList);
+      }
+    };
+    setOnPermissionCallBack(onPermissionCallBack);
+    await onPermission(true);
+  };
 
   return (
     <Card className="w-full">
@@ -37,7 +75,7 @@ const AddZondChainContent = observer(() => {
           className="w-full"
           type="button"
           disabled={isProcessing}
-          onClick={() => onPermission(true)}
+          onClick={() => addBlockchain()}
         >
           <Check className="mr-2 h-4 w-4" />
           Yes
