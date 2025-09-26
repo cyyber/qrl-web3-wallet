5525482c2fcdbb2eaba2f6842ec14a3eeafff544
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-09-26 16:38:45 +0400

    feat: updates onboarding completed

--- src/components/ZondWeb3Wallet/ScreenLoader/Lock/LockPassword/Onboarding/OnboardingCompleted/OnboardingCompleted.tsx
+++ src/components/ZondWeb3Wallet/ScreenLoader/Lock/LockPassword/Onboarding/OnboardingCompleted/OnboardingCompleted.tsx
@@ -7,47 +7,21 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/UI/Card";
-import { useStore } from "@/stores/store";
-import { cva } from "class-variance-authority";
 import { Check } from "lucide-react";
-import { observer } from "mobx-react-lite";
 
-const videoClasses = cva("w-full scale-[2.5]", {
-  variants: {
-    isDarkMode: {
-      true: ["rotate-90"],
-      false: ["rotate-180"],
-    },
-  },
-  defaultVariants: {
-    isDarkMode: false,
-  },
-});
-
-const OnboardingCompleted = observer(() => {
-  const { settingsStore } = useStore();
-  const { isDarkMode, theme } = settingsStore;
-
-  const videoSource = "qrl-video-".concat(theme).concat(".mp4");
-
+const OnboardingCompleted = () => {
   return (
     <Card className="animate-appear-in shadow-xl">
       <CardHeader>
         <CardTitle>That's All</CardTitle>
         <CardDescription className="break-words">
-          The Zond Web3 Wallet is now ready for use.
+          The Zond Web3 Wallet is now ready for use. You can open it from the
+          browser extensions.
         </CardDescription>
       </CardHeader>
       <CardContent>
         <div className="relative flex h-32 w-full overflow-hidden">
-          <div className="absolute inset-0 z-10 flex flex-col justify-end text-lg text-secondary">
-            <div className="opacity-50">We are</div>
-            <div className="font-bold">The Quantum</div>
-            <div className="font-bold">Resistant Ledger</div>
-          </div>
-          <video autoPlay muted loop className={videoClasses({ isDarkMode })}>
-            <source src={videoSource} type="video/mp4" />
-          </video>
+          <img src="onboarding-completed.png" />
         </div>
       </CardContent>
       <CardFooter>
@@ -58,6 +32,6 @@ const OnboardingCompleted = observer(() => {
       </CardFooter>
     </Card>
   );
-});
+};
 
 export default OnboardingCompleted;
