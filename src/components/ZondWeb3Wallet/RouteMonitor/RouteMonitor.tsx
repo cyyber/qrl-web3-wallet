694963e4dda8ad68f89b86c5a10a316c9e25fcd5
Muhammed Shaheer Keerirakath <muhammed.shaheer.keerirakath@gmail.com>
2025-10-01 20:48:48 +0400

    feat: updates route monitor

--- src/components/ZondWeb3Wallet/RouteMonitor/RouteMonitor.tsx
+++ src/components/ZondWeb3Wallet/RouteMonitor/RouteMonitor.tsx
@@ -1,7 +1,5 @@
 import { ROUTES } from "@/router/router";
-import { useStore } from "@/stores/store";
 import StorageUtil from "@/utilities/storageUtil";
-import { observer } from "mobx-react-lite";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
@@ -9,11 +7,7 @@ import { useLocation, useNavigate } from "react-router-dom";
  * An empty component that stays inside the ZondWeb3Wallet component to watch for changing routes.
  * This component takes care of scrolling the screen to top on route change and ensures the user is redirected to the same page on opening the extension.
  */
-const RouteMonitor = observer(() => {
-  const { zondStore } = useStore();
-  const { zondConnection } = zondStore;
-  const { isConnected } = zondConnection;
-
+const RouteMonitor = () => {
   const [previousRouteUsed, setPreviousRouteUsed] = useState(false);
   const navigate = useNavigate();
   const { pathname } = useLocation();
@@ -21,17 +15,16 @@ const RouteMonitor = observer(() => {
   useEffect(() => {
     (async () => {
       const activePage = await StorageUtil.getActivePage();
-      if (activePage && isConnected) {
+      if (activePage) {
         await StorageUtil.clearActivePage();
         setPreviousRouteUsed(true);
         navigate(activePage);
-      } else if (!activePage && isConnected) {
+      } else if (!activePage) {
         setPreviousRouteUsed(true);
-      } else {
         navigate(ROUTES.HOME);
       }
     })();
-  }, [isConnected]);
+  }, []);
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -41,6 +34,6 @@ const RouteMonitor = observer(() => {
   }, [pathname]);
 
   return null;
-});
+};
 
 export default RouteMonitor;
